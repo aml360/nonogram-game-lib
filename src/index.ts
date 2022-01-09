eb4b801d977c2bd240da781a385a4c485f6342fe
aml360 <aml360esp@gmail.com>
2022-01-10 00:28:23 +0100

    style: add .editorconfig file and format the code

--- src/index.ts
+++ src/index.ts
@@ -1,104 +1,104 @@
 export type NonoArray = boolean[][];
 
 export function arrayGenerator(lines: number, columns: number): NonoArray {
-  const miArray = [];
+	const miArray = [];
 
-  for (let i = 0; i < lines; i++) {
-    const columnas = [];
-    for (let j = 0; j < columns; j++) {
-      const booleano = rndBool();
-      columnas.push(booleano);
-    }
+	for (let i = 0; i < lines; i++) {
+		const columnas = [];
+		for (let j = 0; j < columns; j++) {
+			const booleano = rndBool();
+			columnas.push(booleano);
+		}
 
-    miArray.push(columnas);
-  }
+		miArray.push(columnas);
+	}
 
-  return miArray;
+	return miArray;
 }
 
 function rndBool(): boolean {
-  return Math.random() <= 0.5 ? false : true;
+	return Math.random() <= 0.5 ? false : true;
 }
 
 function contabilizador(arrayDatos: boolean[]): number[] {
-  const chunk: number[] = [];
-  let contador = 0;
-  for (const element of arrayDatos) {
-    if (element) {
-      contador++;
-    } else {
-      if (contador > 0) {
-        chunk.push(contador);
-        contador = 0;
-      }
-    }
-  }
-  if (contador > 0) {
-    chunk.push(contador);
-  }
-  return chunk;
+	const chunk: number[] = [];
+	let contador = 0;
+	for (const element of arrayDatos) {
+		if (element) {
+			contador++;
+		} else {
+			if (contador > 0) {
+				chunk.push(contador);
+				contador = 0;
+			}
+		}
+	}
+	if (contador > 0) {
+		chunk.push(contador);
+	}
+	return chunk;
 }
 
 // TODO: Change function name
 export function distribuidor(arrayGenerado: NonoArray) {
-  const distribuidorArr = [];
+	const distribuidorArr = [];
 
-  distribuidorArr.push(distribuidorLineas(arrayGenerado));
+	distribuidorArr.push(distribuidorLineas(arrayGenerado));
 
-  distribuidorArr.push(distribuidorColumnas(arrayGenerado));
+	distribuidorArr.push(distribuidorColumnas(arrayGenerado));
 
-  return distribuidorArr;
+	return distribuidorArr;
 }
 
 // TODO: Change function name
 function distribuidorLineas(arrayGenerado: NonoArray): number[][] {
-  const numerosLineas: number[][] = [];
-  for (const element of arrayGenerado) {
-    const linea = contabilizador(element);
-    numerosLineas.push(linea);
-  }
-  return numerosLineas;
+	const numerosLineas: number[][] = [];
+	for (const element of arrayGenerado) {
+		const linea = contabilizador(element);
+		numerosLineas.push(linea);
+	}
+	return numerosLineas;
 }
 
 // TODO: Change function name
 function distribuidorColumnas(arrayGenerado: NonoArray): number[][] {
-  const numerosColumnas: number[][] = [];
+	const numerosColumnas: number[][] = [];
 
-  for (let i = 0; i < arrayGenerado[0].length; i++) {
-    const numeroColumna = [];
-    for (let j = 0; j < arrayGenerado.length; j++) {
-      const element = arrayGenerado[j][i];
-      numeroColumna.push(element);
-    }
-    const columna = contabilizador(numeroColumna);
-    numerosColumnas.push(columna);
-  }
+	for (let i = 0; i < arrayGenerado[0].length; i++) {
+		const numeroColumna = [];
+		for (let j = 0; j < arrayGenerado.length; j++) {
+			const element = arrayGenerado[j][i];
+			numeroColumna.push(element);
+		}
+		const columna = contabilizador(numeroColumna);
+		numerosColumnas.push(columna);
+	}
 
-  return numerosColumnas;
+	return numerosColumnas;
 }
 
 // TODO: Add tsdoc
 export function isGameCompleted(gameArr: NonoArray, gameSolution: NonoArray) {
-  if (gameArr.length !== gameSolution.length) {
-    throw new Error('Numero filas distinto');
-  }
-  for (const row of gameArr) {
-    for (const rowSolution of gameSolution) {
-      if (row.length !== rowSolution.length) {
-        throw new Error('Numero columnas distinto');
-      }
-    }
-  }
-  let isCompleted = true;
-  loopExterno: for (let i = 0; i < gameArr.length; i++) {
-    for (let j = 0; j < gameArr.length; j++) {
-      const cell = gameArr[i][j];
-      const cellSolution = gameSolution[i][j];
-      if (cell !== cellSolution) {
-        isCompleted = false;
-        break loopExterno;
-      }
-    }
-  }
-  return isCompleted;
+	if (gameArr.length !== gameSolution.length) {
+		throw new Error('Numero filas distinto');
+	}
+	for (const row of gameArr) {
+		for (const rowSolution of gameSolution) {
+			if (row.length !== rowSolution.length) {
+				throw new Error('Numero columnas distinto');
+			}
+		}
+	}
+	let isCompleted = true;
+	loopExterno: for (let i = 0; i < gameArr.length; i++) {
+		for (let j = 0; j < gameArr.length; j++) {
+			const cell = gameArr[i][j];
+			const cellSolution = gameSolution[i][j];
+			if (cell !== cellSolution) {
+				isCompleted = false;
+				break loopExterno;
+			}
+		}
+	}
+	return isCompleted;
 }
